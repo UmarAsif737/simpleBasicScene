9877da238b623379a612560c030a53449d80e555
Umar Asif <umarasif737@gmail.com>
2023-02-12 22:57:53 +0500

    lesson5 animation

--- src/script.js
+++ src/script.js
@@ -9,15 +9,13 @@ const scene = new THREE.Scene();
 const geometry = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.MeshBasicMaterial({ color: "#ff0000" });
 const mesh = new THREE.Mesh(geometry, material);
-mesh.position.x = 0.7;
-mesh.position.y = -0.6;
-mesh.position.z = 1;
-mesh.scale.set(2, 0.5, 0.5);
-mesh.rotation.reorder("YXZ");
-mesh.rotation.set(Math.PI / 4, Math.PI / 4, 0);
+// mesh.position.x = 0.7;
+// mesh.position.y = -0.6;
+// mesh.position.z = 1;
+// mesh.scale.set(2, 0.5, 0.5);
+// mesh.rotation.reorder("YXZ");
+// mesh.rotation.set(Math.PI / 4, Math.PI / 4, 0);
 scene.add(mesh);
-
-console.log(mesh.position.length());
 
 // Sizes
 const sizes = {
