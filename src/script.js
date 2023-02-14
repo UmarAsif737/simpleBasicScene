fe1dbd800430d2c52135baf874f45798fb34e789
Umar Asif <umarasif737@gmail.com>
2023-02-14 16:14:50 +0500

    box made small

--- src/script.js
+++ src/script.js
@@ -27,7 +27,7 @@ const sizes = {
 
 // Camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
-camera.position.z = 3;
+camera.position.z = 3.5;
 // camera.position.x = 1;
 // camera.position.y = 1;
 scene.add(camera);
