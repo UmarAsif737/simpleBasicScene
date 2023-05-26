130aea5fa7b0e1bccf2c4e117eafb6b0c5e4ae9b
Umar Asif <umarasif737@gmail.com>
2023-05-26 21:52:56 +0500

    node modules removed

--- src/script.js
+++ src/script.js
@@ -1,6 +1,7 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import * as dat from "lil-gui";
+
 /**
  * Base
  */
@@ -13,25 +14,90 @@ const canvas = document.querySelector("canvas.webgl");
 // Scene
 const scene = new THREE.Scene();
 
-const image = new Image();
-const texture = new THREE.Texture(image);
-image.addEventListener("load", () => {
-  texture.needsUpdate = true;
-});
-image.src = "/textures/woddtecture1.jpg";
+const textureLoader = new THREE.TextureLoader();
+const woodTexture = textureLoader.load("/textures/woodtexture1.jpg");
+const metalTexture = textureLoader.load("/textures/woodtexture2.jpg");
+const stoneTexture = textureLoader.load("/textures/woodtexture3.jpg");
 
-// Create the attribute and name it 'position'
-const material2 = new THREE.MeshBasicMaterial({
-  map: texture,
-});
+// Materials
+const materials = {
+  wood: new THREE.MeshBasicMaterial({ map: woodTexture }),
+  metal: new THREE.MeshBasicMaterial({ map: metalTexture }),
+  stone: new THREE.MeshBasicMaterial({ map: stoneTexture }),
+};
+
 /**
  * Fonts
  */
 const cubeGeometry = new THREE.BoxGeometry(3, 0.1, 1.5);
 
-const cube = new THREE.Mesh(cubeGeometry, material2);
+const cube = new THREE.Mesh(cubeGeometry, materials.wood);
 
 scene.add(cube);
+
+// GUI Controls
+const guiControls = {
+  texture: "wood",
+  size: { width: 3, height: 0.1 },
+  enableHoles: false,
+};
+
+gui
+  .add(guiControls, "texture", ["wood", "metal", "stone"])
+  .onChange((value) => {
+    cube.material = materials[value];
+  });
+
+gui.add(guiControls.size, "width", 1, 5).onChange((value) => {
+  cube.scale.x = value;
+});
+
+gui.add(guiControls.size, "height", 0.1, 2).onChange((value) => {
+  cube.scale.y = value;
+});
+
+gui.add(guiControls, "enableHoles").onChange((value) => {
+  if (value) {
+    canvas.style.cursor = "crosshair";
+    canvas.addEventListener("mousedown", makeHole);
+  } else {
+    canvas.style.cursor = "default";
+    canvas.removeEventListener("mousedown", makeHole);
+  }
+});
+
+function makeHole(event) {
+  const mouse = new THREE.Vector2();
+  const raycaster = new THREE.Raycaster();
+
+  // Calculate normalized device coordinates
+  mouse.x = (event.clientX / sizes.width) * 2 - 1;
+  mouse.y = -(event.clientY / sizes.height) * 2 + 1;
+
+  // Update the picking ray with the camera and mouse position
+  raycaster.setFromCamera(mouse, camera);
+
+  // Check for intersections
+  const intersects = raycaster.intersectObject(cube);
+
+  if (intersects.length > 0) {
+    // Make a hole in the cube
+    const hole = new THREE.CylinderGeometry(0.2, 0.2, 0.1, 32);
+    const holeMesh = new THREE.Mesh(hole);
+    holeMesh.position.copy(intersects[0].point);
+    holeMesh.position.y += 0.05;
+    holeMesh.rotation.x = Math.PI / 2;
+
+    const mergedGeometry = new THREE.Geometry();
+    mergedGeometry.merge(cube.geometry);
+    mergedGeometry.merge(holeMesh.geometry);
+
+    cube.geometry.dispose(); // Clean up the previous geometry
+    cube.geometry = mergedGeometry;
+    cube.geometry.computeBoundingSphere();
+    cube.geometry.computeVertexNormals();
+  }
+}
 
 /**
  * Sizes
